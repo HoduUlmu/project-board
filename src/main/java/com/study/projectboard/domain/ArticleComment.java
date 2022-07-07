2394f4cafb2b58c74ac9d2d9cbeef11f16cfd53d
HoduUlmu <timewarp96@naver.com>
2022-07-07 20:12:47 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 1 테이블 매칭

--- src/main/java/com/study/projectboard/domain/ArticleComment.java
+++ src/main/java/com/study/projectboard/domain/ArticleComment.java
@@ -1,14 +1,48 @@
 package com.study.projectboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.*;
 
-public class ArticleComment {
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+public class ArticleComment extends Timestamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Article article;
+
+    @Column(nullable = false, length = 500)
     private String content;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @ManyToOne(optional = false,fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @Builder
+    public ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleComment that = (ArticleComment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
